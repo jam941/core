cb4ad909a8cdd1969d39bffca396fef77b744b10
Jarred Moyer <jarredds941@gmail.com>
2023-06-04 22:48:04 -0400

    Generating cards from google sheets DB

--- personalwebsite/src/App.js
+++ personalwebsite/src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './Components/card';
 import axios from 'axios';
 import getJobs from './Services/work';
+import React, { useEffect, useState } from 'react';
+
 //import PublicGoogleSheetsParser from 'public-google-sheets-parser'
 
 function App() {
-  getJobs().then(e=>console.log(e))
+  const [cards, setCards] = useState(['Fuck'])
+  useEffect(()=>{
+    getJobs().then(e=>setCards(e.data.data))
+   
+  },[])
+  
+  
+  const cardBox = cards.map((item,index)=>{
+    return <Card {...item}></Card>
+  })
+
   return (
+    <div>
+    {cards.length>0 ? 
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -21,7 +36,16 @@ function App() {
         >
           Learn React
         </a>
+        
+      
       </header>
+      
+      {cardBox}
+          
+      
+      
+    </div>
+    : <p>Problem</p>}
     </div>
   );
 }
--- personalwebsite/src/Services/work.js
+++ personalwebsite/src/Services/work.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-function getJobs(){
+async function  getJobs(){
     
     const id = '1HFIApiXWF4GFkZhXYddPyUa41n3EktbkDc0hz_x4gMQ'
-    let data = Promise.resolve(axios.get('https://api.fureweb.com/spreadsheets/'+id))
+    let data = await axios.get('https://api.fureweb.com/spreadsheets/'+id)
     return data
+        
+    
 }   
 export default getJobs
