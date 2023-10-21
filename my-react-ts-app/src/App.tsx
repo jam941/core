cc0238cde62c13737ca928fdcd3a73189d13cd13
Jarred Moyer <jarredds941@gmail.com>
2023-10-21 00:06:18 -0400

    first filter button

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -14,12 +14,42 @@ function App() {
   
   
   
+
   const [jobs,setJobs] = useState([]);
-  
+  const [stringData,setStringData] = useState("")
+
   async function  popJobs(){
      getJobs().then(e=>{
-      setJobs(e.data.data)
+      var data = e.data.data
+      setJobs(data)
+      var ble = JSON.stringify({data});
+      setStringData(ble)
      });
+  }
+
+  const [filter,setFilter] = useState("");
+
+  
+
+  const changeFilter = (e :string)=>{
+
+      if(filter === e){
+        setJobs(JSON.parse(stringData).data)
+        setFilter("")
+      }
+      else{
+        setFilter(e)
+        var tempJobs = JSON.parse(stringData).data;
+         tempJobs = tempJobs.filter((item: any)=>{
+          
+            return item.meta.includes(e)
+          
+          })
+          setJobs(tempJobs)
+      }
+      
+    
+
   }
 
   const [start,setStart] = useState(()=>{
@@ -32,7 +62,7 @@ function App() {
     <span className="right"><span className="anim"></span></span>
     </span>)
   }
-  if(jobs.length == 0){
+  if(stringData.length == 0){
     return getSpinner();
   }
   return (
@@ -41,13 +71,23 @@ function App() {
       <div className="w-1/3 ml-10p">
           <Bio/>
       </div>
+      <div>
+      <button
+      className={`border border-white p-2 rounded-lg shadow-md  
+                  ${filter==="ml" ? 'bg-transparent text-white' : 'bg-gray-200'}`}
+      onClick={() => changeFilter("ml")}
+    >
+      Machine Learning
+    </button>
+      </div>
       <div className="w-1/3"></div> {/* Spacer div */}
+      
       <div className="w-1/3 dark mr-10p right overflow-y-auto">
         <div className="py-5 px-10p">
           {jobs && jobs.map((item, index) => <Card key={index} data={item} />)}
         </div>
-          
       </div>
+      
     </div>
   );
   //<div className="w-1/2 space-y-1 ">
