5d7a504143116f72eb3a2066290733dcc352b56b
jam941 <jarredds941@gmail.com>
2025-05-25 18:27:23 -0500

    Attempts at a browser tab

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -26,6 +26,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
